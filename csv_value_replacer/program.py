723b6e218e04934c0883de2d53457e24b8ababd7
Manuel Galli <gmanuel89@gmail.com>
2022-04-29 17:49:28 +0200

    added checks

--- csv_value_replacer/program.py
+++ csv_value_replacer/program.py
@@ -8,6 +8,25 @@ from tkinter import *
 from tkinter import Entry, messagebox, filedialog
 import csv
 
+# Input map CSV file (with replacing map --> "Old value" ; "New value")
+Tk().withdraw()
+messagebox.showinfo(title='Select map CSV file', message='Select the CSV file with the "old"-"new" map for value replacement')
+Tk().withdraw()
+input_csv_file_with_map = filedialog.askopenfilename(filetypes=[('CSV files', '.csv')])
+Tk().withdraw()
+messagebox.showinfo(title='CSV file selected', message="The CSV file selected is '%s'" % (input_csv_file_with_map))
+# Try to read map only if a file is selected
+if input_csv_file_with_map != "":
+    # Open CSV with mapping
+    with open(input_csv_file_with_map, 'r', encoding='UTF8') as input_file:
+        input_csv_file_with_map_lines = list(csv.reader(input_file))
+    # Create the map
+    mapping_dictionary_array = create_replacing_map(input_csv_file_with_map_lines)
+else :
+    input_csv_file_with_map_lines = []
+    mapping_dictionary_array = []
+
+
 # Input CSV file (to be replaced)
 Tk().withdraw()
 messagebox.showinfo(title='Select CSV file', message='Select the CSV file with values to be replaced')
@@ -15,34 +34,19 @@ Tk().withdraw()
 input_csv_file = filedialog.askopenfilename(filetypes=[('CSV files', '.csv')])
 Tk().withdraw()
 messagebox.showinfo(title='CSV file selected', message="The CSV file selected is '%s'" % (input_csv_file))
-# Read the input CSV
-with open(input_csv_file, 'r', encoding='UTF8') as input_file:
-    # Read CSV file lines
-    input_csv_file_lines = list(csv.reader(input_file))
-
-# Input CSV file (with replacing map --> "Old value" ; "New value")
-Tk().withdraw()
-messagebox.showinfo(title='Select map CSV file', message='Select the CSV file with the "old"-"new" map for value replacement')
-Tk().withdraw()
-input_csv_file_with_map = filedialog.askopenfilename(filetypes=[('CSV files', '.csv')])
-Tk().withdraw()
-messagebox.showinfo(title='CSV file selected', message="The CSV file selected is '%s'" % (input_csv_file_with_map))
-# Open CSV with mapping
-with open(input_csv_file_with_map, 'r', encoding='UTF8') as input_file:
-    input_csv_file_with_map_lines = list(csv.reader(input_file))
-
-# Create the map
-mapping_dictionary_array = create_replacing_map(input_csv_file_with_map_lines)
-
-# Generate the output
-output_csv_file_lines = replace_csv_values(input_csv_file_lines, mapping_dictionary_array)
-
-# Write the output file onto the input file   
-with open (input_csv_file, 'w+', encoding='UTF8', newline='') as output_file:
-    csv_writer = csv.writer(output_file)
-    for line in output_csv_file_lines:
-            csv_writer.writerow(line)
-
-# Success
-Tk().withdraw()
-messagebox.showinfo(title='Success', message="The selected the CSV file:\n'%s'\nwas successfully processed" % (input_csv_file))
+# Run only if a file is selected
+if input_csv_file_with_map != "":
+    # Read the input CSV
+    with open(input_csv_file, 'r', encoding='UTF8') as input_file:
+        # Read CSV file lines
+        input_csv_file_lines = list(csv.reader(input_file))
+    # Generate the output
+    output_csv_file_lines = replace_csv_values(input_csv_file_lines, mapping_dictionary_array)
+    # Write the output file onto the input file   
+    with open (input_csv_file, 'w+', encoding='UTF8', newline='') as output_file:
+        csv_writer = csv.writer(output_file)
+        for line in output_csv_file_lines:
+                csv_writer.writerow(line)
+    # Success
+    Tk().withdraw()
+    messagebox.showinfo(title='Success', message="The selected the CSV file:\n'%s'\nwas successfully processed" % (input_csv_file))
--- csv_value_replacer/replace_csv_values.py
+++ csv_value_replacer/replace_csv_values.py
@@ -1,5 +1,7 @@
 # Replace the csv values according to a list of dictionaries with 'old' and 'new' values
 def replace_csv_values(input_csv_file_lines, mapping_dictionary_array):
+    # Run if there is a map (otherwise return the input file with no modifications)
+    if len(mapping_dictionary_array) == 0: return input_csv_file_lines
     # For each cell...
     for r in range(len(input_csv_file_lines)):
         for c in range(len(input_csv_file_lines[r])):
