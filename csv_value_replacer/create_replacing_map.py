84fafcb1d63ca41d8025ff6bade9e8ab0c134974
Manuel Galli <gmanuel89@gmail.com>
2022-05-06 17:11:05 +0200

    added possibility to define column(s) to replace values in

--- csv_value_replacer/create_replacing_map.py
+++ csv_value_replacer/create_replacing_map.py
@@ -7,17 +7,32 @@ def create_replacing_map(input_csv_lines):
     # Determine the indices of the "new" and the "old"
     new_index = None
     old_index = None
+    column_index = None
     for i in range(len(csv_header)):
         if 'old' in csv_header[i].lower():
             old_index = i
     for i in range(len(csv_header)):
         if 'new' in csv_header[i].lower():
             new_index = i
+    for i in range(len(csv_header)):
+        if 'column' in csv_header[i].lower():
+            column_index = i
     # Build the list of dictionaries
     mapping_dictionary_array = []
-    if new_index is not None and old_index is not None:
+    # If the "column" is not specified
+    if new_index is not None and old_index is not None and column_index is None:
         for i in range(len(input_csv_lines)):
             mapping_dictionary = {'old' : input_csv_lines[i][old_index], 'new' : input_csv_lines[i][new_index]}
             mapping_dictionary_array.append(mapping_dictionary)
+    # If the "column" is specified
+    elif new_index is not None and old_index is not None and column_index is not None:
+        for i in range(len(input_csv_lines)):
+            # retrieve the individual column names (stripped from spaces)
+            column_indices = input_csv_lines[i][column_index].split(',')
+            for c in range(len(column_indices)):
+                column_indices[c] = column_indices[c].strip()
+            mapping_dictionary = {'old' : input_csv_lines[i][old_index], 'new' : input_csv_lines[i][new_index], 'columns' : column_indices}
+            mapping_dictionary_array.append(mapping_dictionary)
     # Return
+    print(mapping_dictionary_array)
     return mapping_dictionary_array
--- csv_value_replacer/replace_csv_values.py
+++ csv_value_replacer/replace_csv_values.py
@@ -2,12 +2,43 @@
 def replace_csv_values(input_csv_file_lines, mapping_dictionary_array):
     # Run if there is a map (otherwise return the input file with no modifications)
     if len(mapping_dictionary_array) == 0: return input_csv_file_lines
-    # For each cell...
-    for r in range(len(input_csv_file_lines)):
-        for c in range(len(input_csv_file_lines[r])):
-            # See if there is a match with the mapping array
-            for map in mapping_dictionary_array:
-                if str(input_csv_file_lines[r][c]) == str(map.get("old")):
-                    input_csv_file_lines[r][c] = str(map.get("new"))
+    # See if there is a match with the mapping array
+    for maprepl in mapping_dictionary_array:
+        # If there is no column(s) specified, go for the all-cell replacement
+        if maprepl.get('columns') is None:
+            # For each row...
+            for r in range(len(input_csv_file_lines)):
+                # Skip the header
+                if r == 0 : continue
+                # For each column
+                for c in range(len(input_csv_file_lines[r])):     
+                    # Replace the cell value
+                    if str(input_csv_file_lines[r][c]) == str(maprepl.get('old')):
+                        input_csv_file_lines[r][c] = str(maprepl.get('new'))
+        else:
+            # Determine the indices of the columns (compare mapping with the header)
+            columns = maprepl.get('columns')
+            column_indices = []
+            for i in range(len(columns)):
+                for c in range(len(input_csv_file_lines[0])):
+                    if columns[i] == input_csv_file_lines[0][c]:
+                        column_indices.append(c)
+            # Replace all the values only in selected columns
+            # For each row...
+            for r in range(len(input_csv_file_lines)):
+                # Skip the header
+                if r == 0 : continue
+                # For each column
+                if len(column_indices) == 0:
+                    for c in range(len(input_csv_file_lines[r])):     
+                        # Replace the cell value
+                        if str(input_csv_file_lines[r][c]) == str(maprepl.get('old')):
+                            input_csv_file_lines[r][c] = str(maprepl.get('new'))
+                else:
+                    # For each column (to replace)
+                    for c in column_indices:     
+                        # Replace the cell value
+                        if str(input_csv_file_lines[r][c]) == str(maprepl.get('old')):
+                            input_csv_file_lines[r][c] = str(maprepl.get('new'))  
     # Return
     return input_csv_file_lines
