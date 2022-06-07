4addebaf9fbf9133ed08d8bfaa1bb03a163c6116
Manuel Galli <gmanuel89@gmail.com>
2022-06-07 17:53:28 +0200

    updated csv header retrieval in case of list

--- functions/get_csv_header.py
+++ functions/get_csv_header.py
@@ -1,3 +1,8 @@
 ## Get CSV header
 def get_csv_header(input_csv_file):
-    return input_csv_file[0]
+    if isinstance(input_csv_file[0], list):
+        return input_csv_file[0]
+    elif isinstance(input_csv_file[0], dict):
+        return input_csv_file[0].keys()
+    else:
+        pass
