434273fb072cd6658690721a9034338c4c96835c
Manuel Galli <gmanuel89@gmail.com>
2022-06-14 10:52:24 +0200

    fixed reading CSV file function

--- functions/read_csv_file.py
+++ functions/read_csv_file.py
@@ -3,8 +3,8 @@ import csv
 import sys
 
 ## Read input csv file (returns a list of rows)
-def read_csv_file(input_csv_file, output_format='list'):
-    # Open the CSV file and convert it to a list
+def read_csv_file(input_csv_file: str, output_format='list'):
+    ## Open the CSV file
     with open(input_csv_file, 'r', encoding='UTF8') as input_file:
         # Prevent possible errors due to large columns (beyond 131072 characters)
         try:
@@ -35,5 +35,15 @@ def read_csv_file(input_csv_file, output_format='list'):
                     input_csv_file_lines.append(dict(row))
             else:
                 input_csv_file_lines = csv.reader(input_file)
+    ## Bring the row lengths on par
+    if output_format == 'list':
+        csv_column_header = input_csv_file_lines[0]
+        csv_column_number = len(csv_column_header)
+        for r in range(len(input_csv_file_lines)):
+            if len(input_csv_file_lines[r]) < csv_column_number:
+                for cdiff in range(csv_column_number - len(input_csv_file_lines[r])):
+                    input_csv_file_lines[r].append(None)
+    else:
+        pass
     # return
     return input_csv_file_lines
